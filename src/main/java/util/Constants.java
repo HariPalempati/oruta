8923aeae4755f2ccb16e20d6541681115c444877
PALEMPATI <harikrishna.palempati@gmail.com>
2015-03-28 23:01:30 -0400

    login added

--- src/main/java/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public class Constants {
-	public static class Pagination {
-		public static String DATA = "data";
-		public static String OFFSET = "offset";
-		public static String COUNT = "count";
-	}
-}
