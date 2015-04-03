394cdb37611bc463dba7a27a14c06bb9c0e91fed
PALEMPATI <harikrishna.palempati@gmail.com>
2015-04-03 04:15:14 -0400

    Modified

--- src/main/java/command/UploadCommand.java
+++ src/main/java/command/UploadCommand.java
@@ -19,9 +19,9 @@ public class UploadCommand {
 			byte[] filecontent = output.toByteArray();
 			Connection connection = ConnectionProvider.getConnection();
 			PreparedStatement stmt = connection
-					.prepareStatement("INSERT INTO Upload VALUES (?, ?)");
-			stmt.setString(1, Username);
-			stmt.setBytes(2, FileName);
+					.prepareStatement("INSERT INTO  VALUES (?, ?)");
+			stmt.setString(1, fileName);
+			stmt.setBytes(2, filecontent);
 			stmt.executeUpdate();
 
 		} catch (Exception e) {
